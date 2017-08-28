f8f2e6e5179ce71e240680f67eea730c43f6c04e
Viktor <pulyakviktor@gmail.com>
2017-08-28 15:17:17 +0300

    add transfers method

--- repositories/Erc20TransferRepository.js
+++ repositories/Erc20TransferRepository.js
@@ -3,12 +3,36 @@ const Erc20Transfer = require('../models/Erc20Transfer');
 
 function Erc20TransferRepository () {}
 
+/**
+ *
+ * @param {String} contractAddress
+ * @param {Function} next
+ * @return {*}
+ */
 Erc20TransferRepository.prototype.getCountTransfers = function (contractAddress, next) {
     return Erc20Transfer.count({contract_address: contractAddress}, function(err, count) {
         return next(err, count);
     });
 };
 
+/**
+ *
+ * @param {String} contractAddress
+ * @param {Object} options
+ * @param {Number} options.limit
+ * @param {Number} options.offset
+ * @param {Function} next
+ */
+Erc20TransferRepository.prototype.fetchTransfers = function (contractAddress, options, next) {
+    return Erc20Transfer.find({contract_address: contractAddress}, {}, {sort: {created_at: -1}, limit: options.limit, skip: options.offset}, function(err, transfers) {
+        return next(err, transfers);
+    });
+};
+/**
+ *
+ * @param {Object} data
+ * @param {Function} next
+ */
 Erc20TransferRepository.prototype.createOrUpdateTx = function (data, next) {
     return Erc20Transfer.findOneAndUpdate({tx_hash: data.tx_hash, log_idx: data.log_idx, contract_address: data.contract_address}, data, {upsert: true, new: true}, function(err, row) {
         return next(err, row);
