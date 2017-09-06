d81043d0816b81da676976bc23f1ed802532e009
Viktor <pulyakviktor@gmail.com>
2017-09-06 17:56:22 +0300

    add qtum-core

--- helpers/ContractsHelper.js
+++ helpers/ContractsHelper.js
@@ -1,4 +1,4 @@
-var bitcore = require('bitcore-lib');
+var bitcore = require('qtumcore-lib');
 var functionHashes = require("../data/contracts/erc20/FunctionHashes.json");
 var CONTRACT_CALL = 194;
 var CONTRACT_CREATE = 193;
